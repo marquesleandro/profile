eb1481a052a4912c0ccd83a46f3f0700a6c63561
marquesleandro <marquesleandro67@gmail.com>
2020-05-11 15:39:35 -0300

    reorganized validation folder

---
+++ cavity/100Re/re100_vx.py
@@ -0,0 +1,130 @@
+# =======================
+# Importing the libraries
+# =======================
+
+import sys
+
+import numpy as np
+from tqdm import tqdm
+from time import time
+import matplotlib.pyplot as plt
+
+
+print '------------'
+print 'COMPARATION:'
+print '------------'
+
+start_time = time()
+
+# -----------
+# GHIA et al.
+# -----------
+
+y_a = np.zeros([17,1], dtype = float)
+u_a = np.zeros([17,1], dtype = float)
+
+
+y_a[16] = 1.0000
+y_a[15] = 0.9766
+y_a[14] = 0.9688
+y_a[13] = 0.9609
+y_a[12] = 0.9531
+y_a[11] = 0.8516
+y_a[10] = 0.7344
+y_a[9] = 0.6172
+y_a[8] = 0.5000
+y_a[7] = 0.4531
+y_a[6] = 0.2813
+y_a[5] = 0.1719
+y_a[4] = 0.1016
+y_a[3] = 0.0703
+y_a[2] = 0.0625
+y_a[1] = 0.0547
+y_a[0] = 0.0000
+
+u_a[16] = 1.00000
+u_a[15] = 0.84123 
+u_a[14] = 0.78871 
+u_a[13] = 0.73722 
+u_a[12] = 0.68717 
+u_a[11] = 0.23151 
+u_a[10] = 0.00332 
+u_a[9] = -0.13641 
+u_a[8] = -0.20581 
+u_a[7] = -0.21090 
+u_a[6] = -0.15662 
+u_a[5] = -0.10150 
+u_a[4] = -0.06434 
+u_a[3] = -0.04775 
+u_a[2] = -0.04192 
+u_a[1] = -0.03717 
+u_a[0] = 0.00000
+
+# -------------
+# MARCHI et al.
+# -------------
+
+y_b = np.zeros([17,1], dtype = float)
+u_b = np.zeros([17,1], dtype = float)
+
+y_b[16] = 1.0000
+y_b[15] = 0.9375
+y_b[14] = 0.8750
+y_b[13] = 0.8125
+y_b[12] = 0.7500
+y_b[11] = 0.6875
+y_b[10] = 0.6250
+y_b[9] = 0.5625
+y_b[8] = 0.5000
+y_b[7] = 0.4375
+y_b[6] = 0.3750
+y_b[5] = 0.3125
+y_b[4] = 0.2500
+y_b[3] = 0.1875
+y_b[2] = 0.1250
+y_b[1] = 0.0625
+y_b[0] = 0.0000
+
+u_b[16] = 1.00000
+u_b[15] = 0.59746 
+u_b[14] = 0.31055 
+u_b[13] = 0.14042 
+u_b[12] = 0.02787 
+u_b[11] = -0.06024 
+u_b[10] = -0.13125
+u_b[9] = -0.18208 
+u_b[8] = -0.20914 
+u_b[7] = -0.21296 
+u_b[6] = -0.19847 
+u_b[5] = -0.17271
+u_b[4] = -0.14193 
+u_b[3] = -0.10981 
+u_b[2] = -0.07712 
+u_b[1] = -0.04197 
+u_b[0] = 0.00000
+
+
+cav_num = []
+with open('ALEre100eLagrangian_vx.csv') as cav:
+ for line in cav:
+  row = line.split(',')
+  cav_num.append(row[:])
+
+y = np.zeros([len(cav_num)-1,1], dtype = float)
+vx = np.zeros([len(cav_num)-1,1], dtype = float)
+for i in range(1,len(cav_num)):
+ vx[i-1] = cav_num[i][1]
+ y[i-1] = cav_num[i][7]
+
+
+end_time = time()
+print 'time duration: %.1f seconds' %(end_time - start_time)
+print ""
+
+plt.plot(vx, y, '-', color='black', label = "current work")
+plt.plot(u_a, y_a, 'o', color='black', label = "Ghia et al. (1982)")
+plt.plot(u_b, y_b, 'x', color='black', label = "Marchi et al. (2009)")
+plt.legend(loc = 4)
+plt.ylabel('y')
+plt.xlabel('u-velocity\n\n(a)')
+plt.show()
---
+++ poiseuille/poiseulle_1c.py
@@ -0,0 +1,118 @@
+# =======================
+# Importing the libraries
+# =======================
+
+import sys
+sys.path.insert(0, '/home/marquesgesar/fem/lib_class')
+
+import numpy as np
+import scipy.sparse as sps
+import scipy.sparse.linalg
+import scipy.linalg
+import trimsh
+import trielem
+from tricond import b_bc
+import InOut
+from tqdm import tqdm
+from time import time
+import matplotlib.pyplot as plt
+
+print '------------'
+print 'COMPARATION:'
+print '------------'
+
+start_time = time()
+
+# -----
+# 0.1 s
+# -----
+
+cav_num = []
+with open('poiseulle_20c.csv') as cav:
+ for line in cav:
+  row = line.split(',')
+  cav_num.append(row[:])
+
+y1 = np.zeros([len(cav_num)-1,1], dtype = float)
+vx1 = np.zeros([len(cav_num)-1,1], dtype = float)
+for i in range(1,len(cav_num)):
+ vx1[i-1] = cav_num[i][1]
+ y1[i-1] = cav_num[i][7]
+
+# -----
+# 0.2 s
+# -----
+
+cav_num = []
+with open('poiseulle_40c.csv') as cav:
+ for line in cav:
+  row = line.split(',')
+  cav_num.append(row[:])
+
+y2 = np.zeros([len(cav_num)-1,1], dtype = float)
+vx2 = np.zeros([len(cav_num)-1,1], dtype = float)
+for i in range(1,len(cav_num)):
+ vx2[i-1] = cav_num[i][1]
+ y2[i-1] = cav_num[i][7]
+
+
+# -----
+# 1 s
+# -----
+
+cav_num = []
+with open('poiseulle_200c.csv') as cav:
+ for line in cav:
+  row = line.split(',')
+  cav_num.append(row[:])
+
+y3 = np.zeros([len(cav_num)-1,1], dtype = float)
+vx3 = np.zeros([len(cav_num)-1,1], dtype = float)
+for i in range(1,len(cav_num)):
+ vx3[i-1] = cav_num[i][1]
+ y3[i-1] = cav_num[i][7]
+
+# ------------
+# Steady State
+# ------------
+
+cav_num = []
+with open('poiseulle_1500c.csv') as cav:
+ for line in cav:
+  row = line.split(',')
+  cav_num.append(row[:])
+
+y4 = np.zeros([len(cav_num)-1,1], dtype = float)
+vx4 = np.zeros([len(cav_num)-1,1], dtype = float)
+for i in range(1,len(cav_num)):
+ vx4[i-1] = cav_num[i][1]
+ y4[i-1] = cav_num[i][7]
+
+
+# -----
+# exact
+# -----
+vxe = np.zeros([200,1], dtype = float)
+ye = np.zeros([200,1], dtype = float)
+
+u_max = 1.5
+L = 1.0
+for i in range(0,200):
+ ye[i] = (i/200.0)
+ vxe[i] = ((4.0*u_max*ye[i])/L**2)*(L - ye[i])
+
+
+
+end_time = time()
+print 'time duration: %.1f seconds' %(end_time - start_time)
+print ""
+
+
+plt.plot(vx1, y1, '2', color='black', label = "t = 0.1")
+plt.plot(vx3, y3, '.', color='black', fillstyle='none', label = "t = 1.0")
+plt.plot(vx4, y4, '-', color='black', label = "solucao numerica")
+plt.plot(vxe, ye, '--', color='black', label = "solucao analitica")
+plt.legend(loc = 3)
+plt.ylabel('y')
+plt.xlabel('velocidade-u')
+plt.show()
