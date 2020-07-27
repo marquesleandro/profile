45064c8cb026d72c46170f97daf720caf5d16699
marquesleandro <marquesleandro67@gmail.com>
2020-07-27 14:59:06 -0300

    add exact solution convection

--- convection/convection.py
+++ convection/convection.py
@@ -52,16 +52,29 @@ for i in range(1,len(cav_num)):
 # -----
 
 cav_num = []
-with open('convection_exact.csv') as cav:
+with open('convection_exact10.csv') as cav:
  for line in cav:
   row = line.split(',')
   cav_num.append(row[:])
 
-y3 = np.zeros([len(cav_num)-1,1], dtype = float)
-vx3 = np.zeros([len(cav_num)-1,1], dtype = float)
+ye0 = np.zeros([len(cav_num)-1,1], dtype = float)
+vxe0 = np.zeros([len(cav_num)-1,1], dtype = float)
+ye1 = np.zeros([len(cav_num)-1,1], dtype = float)
+vxe1 = np.zeros([len(cav_num)-1,1], dtype = float)
+ye2 = np.zeros([len(cav_num)-1,1], dtype = float)
+vxe2 = np.zeros([len(cav_num)-1,1], dtype = float)
+ye3 = np.zeros([len(cav_num)-1,1], dtype = float)
+vxe3 = np.zeros([len(cav_num)-1,1], dtype = float)
+
 for i in range(1,len(cav_num)):
- vx3[i-1] = cav_num[i][4]
- y3[i-1] = cav_num[i][6]
+ vxe0[i-1] = cav_num[i][3]
+ vxe1[i-1] = cav_num[i][4]
+ vxe2[i-1] = cav_num[i][5]
+ vxe3[i-1] = cav_num[i][6]
+ ye0[i-1] = cav_num[i][7]
+
+
+
 
 end_time = time()
 print 'time duration: %.1f seconds' %(end_time - start_time)
@@ -69,7 +82,10 @@ print ""
 
 plt.plot(y1, vx1, '--', color='black', label = "Galerkin")
 plt.plot(y2, vx2, '-', color='black', label = "Taylor-Galerkin")
-#plt.plot(y3, vx3, '--', color='black', label = "solucao analitica")
+plt.plot(ye0, vxe0, '--', color='black', label = "solucao analitica")
+plt.plot(ye0, vxe1, '--', color='black', label = "solucao analitica")
+plt.plot(ye0, vxe2, '--', color='black', label = "solucao analitica")
+plt.plot(ye0, vxe3, '--', color='black', label = "solucao analitica")
 plt.legend(loc = 1)
 plt.ylabel('escalar c')
 plt.xlabel('posicao')
